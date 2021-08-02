57bd78160c245ebfb2a4c67e53f0fef413d25168
gitgeoman <gitgeoman@gmail.com>
2021-08-02 15:25:32 +0200

    react router & maps

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import "./App.css";
-import Map from "./Map";
-import { useState } from "react";
-import Legenda from "./Legenda";
-import data from "./dane_";
+
+import PureMap from "./Map";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import budynki from "./spatialData/budynki.json";
+import militaryUnits from "./spatialData/MilitaryUnits";
 
 function App() {
-  const [wydzialy, setWydzialy] = useState(data);
-  const [selected, setSelected] = useState(null);
-  const [color, setColor] = useState(null);
   return (
     <div className="app">
-      <h3>Mapa WAT</h3>
-      <div className="app_wszystkieWydzialy">
-        {wydzialy.map((item) => {
-          // console.log(item);
-          return (
-            <div
-              className="app_wydzial"
-              key={item.id}
-              onMouseOver={() => {
-                setSelected(item.name);
-                setColor(item.hex);
-                // console.log(selected);
-              }}
-              onMouseOut={() => {
-                setSelected("");
-                setColor(null);
-                // console.log(selected);
-              }}
-            >
-              <img className="img" src={item.logo} alt="" />
-            </div>
-          );
-        })}
-      </div>
-
-      <Legenda />
-      <Map selected={selected} color={color} />
+      <h3>strona główna</h3>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/MilitaryUnitsMap">Polish Military Units Map</Link>
+              </li>
+              <li>
+                <Link to="/MUTMap">MutMap</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/MilitaryUnitsMap">
+              <PureMap data={militaryUnits} zoom={6} />
+            </Route>
+            <Route path="/MUTMap">
+              <PureMap data={budynki} zoom={15} />
+            </Route>
+            <Route path="/"></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -2,66 +2,17 @@ import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "./Map.css";
 
-import budynki from "./spatialData/budynki.json";
-
-function Map({ selected, color }) {
-  console.log(selected);
-  // budynki.features.map((item) => {
-  //   console.log(item.properties.namePL == selected ? "tak" : "nie");
-  // });
-  let filtered = budynki.features.filter(
-    (item) => item.properties.namePL === selected
-  );
-  console.log(filtered);
-
-  // create map
+function Map({ data, zoom }) {
+  // // create map
   const mapRef = useRef(null);
 
   useEffect(() => {
     mapRef.current = L.map("map", {
       center: [52.253, 20.9],
-      zoom: 15,
+      zoom: zoom,
       layers: [L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png")],
     });
-  }, []);
-
-  //add next layer to map
-  const budynkiLayer = useRef(null); //creating empty layer
-  useEffect(() => {
-    budynkiLayer.current = L.layerGroup().addTo(mapRef.current);
-  }, []);
-
-  //updating data inside the layer
-  useEffect(() => {
-    budynkiLayer.current.clearLayers();
-    L.geoJSON(budynki, {
-      style: {
-        weight: 2,
-        opacity: 1,
-        color: "gray",
-        dashArray: "3",
-        fillOpacity: 0.7,
-        fillColor: "#FFA000",
-      },
-    }).addTo(budynkiLayer.current);
-  }, []);
-
-  //add layer
-  const buildingSelection = useRef(null); //creating empty layer
-  useEffect(() => {
-    buildingSelection.current?.clearLayers();
-    buildingSelection.current = L.layerGroup().addTo(mapRef.current);
-    L.geoJSON(filtered, {
-      style: {
-        weight: 2,
-        opacity: 1,
-        color: "blue",
-        dashArray: "3",
-        fillOpacity: 0.7,
-        fillColor: "#111111",
-      },
-    }).addTo(buildingSelection.current);
-  }, [selected, filtered]);
+  }, [zoom]);
 
   return <div id="map"></div>;
 }
